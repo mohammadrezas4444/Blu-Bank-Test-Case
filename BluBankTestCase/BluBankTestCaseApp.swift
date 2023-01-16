0514539be11e5d2ebbce256603d773d727d17b7e
Mohammad Reza <kattieharrison5551@gmail.com>
2023-01-16 22:37:38 +0330

    first commit

---
+++ BluBankTestCase/BluBankTestCaseApp.swift
@@ -0,0 +1,17 @@
+//
+//  BluBankTestCaseApp.swift
+//  BluBankTestCase
+//
+//  Created by Mohammad Reza Sohrabi on 1/16/23.
+//
+
+import SwiftUI
+
+@main
+struct BluBankTestCaseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
