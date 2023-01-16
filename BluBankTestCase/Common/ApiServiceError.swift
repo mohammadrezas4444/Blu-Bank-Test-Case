9ea65e69e5ce3cd8415fd0e79a1c280ba34e31da
Mohammad Reza <kattieharrison5551@gmail.com>
2023-01-17 03:19:04 +0330

    Some code improvement

---
+++ BluBankTestCase/Common/ApiServiceError.swift
@@ -0,0 +1,8 @@
+//
+//  ApiServiceError.swift
+//  BluBankTestCase
+//
+//  Created by Mohammad Reza Sohrabi on 1/17/23.
+//
+
+import Foundation
---
+++ BluBankTestCase/Common/UseCaseError.swift
@@ -0,0 +1,8 @@
+//
+//  UseCaseError.swift
+//  BluBankTestCase
+//
+//  Created by Mohammad Reza Sohrabi on 1/17/23.
+//
+
+import Foundation
